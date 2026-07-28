5c1f0a65332b5383ab70757dc956de6087ca16ef
Sanftperlig <291568973+Sanftperlig@users.noreply.github.com>
2026-07-28 17:06:28 +0200

    Preserve explicit 0 charge state in supercell IStructure __mul__

--- src/pymatgen/core/structure.py
+++ src/pymatgen/core/structure.py
@@ -1192,7 +1192,7 @@ class IStructure(SiteCollection[PeriodicSite], MSONable):
                 )
                 new_sites.append(periodic_site)
 
-        new_charge = self._charge * np.linalg.det(scale_matrix) if self._charge else None
+        new_charge = self._charge * np.linalg.det(scale_matrix) if self._charge is not None else None
         return Structure.from_sites(new_sites, charge=new_charge, to_unit_cell=True).relabel_sites(ignore_uniq=True)
 
     def __rmul__(self, scaling_matrix):
